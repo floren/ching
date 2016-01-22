a7e11c9cfa6b06047b30d0beabeb439b72c69ace
John Floren <john@jfloren.net>
2016-01-22 09:19:10 -0700

    Initial import

---
+++ phx/pathnames.h
@@ -0,0 +1,10 @@
+/*-
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	8.1 (Berkeley) 05/31/93
+ */
+
+#define	_PATH_HEX	"/usr/local/share/games/ching/hexagrams"
